d4f2377d6dd9b79e0210c7a47b877210fc8f7114
Zheng Hao Tan <hi@zhenghaotan.com>
2024-02-08 12:38:10 -0800

    Add comments in .fzf.zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -6,9 +6,11 @@ fi
 
 # Auto-completion
 # ---------------
+# (or find where this completion.zsh file is)
 [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
+# (or find where this key-bindings.zsh file is)
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 
